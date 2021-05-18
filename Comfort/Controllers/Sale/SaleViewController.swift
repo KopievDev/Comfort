74f1d943caf534798b98910f0383f0939b600096
Ivan Kopiev <x@mustdev.ru>
2021-05-18 22:45:31 +0300

    create UI

---
+++ Comfort/Controllers/Sale/SaleViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SaleViewController.swift
+//  Comfort
+//
+//  Created by Ivan Kopiev on 18.05.2021.
+//
+
+import UIKit
+
+class SaleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "Продажа"
+        navigationItem.leftBarButtonItem?.title = "Выйти"
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
